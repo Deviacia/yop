16197fff47a56ca6dd3b05c0a01edfd2ca2ab9a3
Deviacia <ivan.bpemr@gmail.com>
2025-04-08 23:10:21 +0300

    chore(layout): Subscription layout fix
    Subscription header: Small yopta fix related to width and positoin steeper component

--- src/modules/subscriptions/ui/subscription-header.tsx
+++ src/modules/subscriptions/ui/subscription-header.tsx
@@ -2,15 +2,17 @@ import { SubscriptionStepper } from './subscription-stepper'
 
 export function SubscriptionHeader() {
   return (
-    <header className="flex flex-col gap-y-7">
-      <div>
-        <p className="text-center text-xl font-semibold">MyLuck</p>
-        <p className="text-center text-sm text-gray-500">
-          Velg ditt abonnement
-        </p>
-      </div>
+    <header className='flex justify-center'>
+      <div className="flex flex-col gap-y-7 w-[245px]">
+        <div>
+          <p className="text-center text-xl font-semibold">MyLuck</p>
+          <p className="text-center text-sm text-gray-500">
+            Velg ditt abonnement
+          </p>
+        </div>
 
-      <SubscriptionStepper />
+        <SubscriptionStepper />
+      </div>
     </header>
   )
 }
